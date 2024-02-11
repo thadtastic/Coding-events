945077b68db46ab7ef96cd26ec5f452418a04599
thadtastic <leothaddeus92@gmail.com>
2024-02-11 14:57:24 -0600

    program breaks when fragments or implemented

--- codingevents/src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ codingevents/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -17,6 +17,7 @@ public class EventController {
     private static List<String> events = new ArrayList<>();
 
     //handler method for GET requests wth single parameter
+    // root path is defined at the class leve with @GetMapping("events")
 @GetMapping("/")
     public String displayAllEvents(Model model){
         model.addAttribute("events", events);
@@ -28,11 +29,12 @@ public class EventController {
     return "events/create";
     }
     // method below handles a different kind of request and can be placed at the same path
+    // processes form after user clicks submit, PostMapping annotation needed for this
 @PostMapping("create")
-public String createEvent(@RequestParam String eventName){
+public String createEvent(@RequestParam String eventName){ // eventName must match name attribute of input elements:  <input type ="text" name="eventName">
         events.add(eventName);
         //the return redirects to the root path, which is events
-        return "redirect: ";
+        return "redirect:";
     }
 
 }
