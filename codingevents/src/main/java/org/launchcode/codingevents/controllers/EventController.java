b011018f0f09c6e3d2eb9d544081e2c851ccd900
thadtastic <leothaddeus92@gmail.com>
2024-02-11 10:58:43 -0600

    First commit, coding events setup completed

---
+++ codingevents/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -0,0 +1,25 @@
+package org.launchcode.codingevents.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("events")
+public class EventController {
+
+    //handler method for GET requests wth single parameter
+@GetMapping
+    public String requests(Model model){
+        List<String> events = new ArrayList<>();
+        events.add("Fun Mudder");
+        events.add("Rise and Grind");
+        events.add("The Road to Victory");
+        model.addAttribute("events", "events");
+        return "events/index";
+    }
+}
