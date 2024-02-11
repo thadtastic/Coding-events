718d001043346d09dcbba82bcd72bc3370a7930f
thadtastic <leothaddeus92@gmail.com>
2024-02-11 13:30:57 -0600

    fixed form render after input is submitted

--- codingevents/src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ codingevents/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,14 +14,12 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    private static List<String> events = new ArrayList<>();
+
     //handler method for GET requests wth single parameter
-@GetMapping
-    public String requests(Model model){
-        List<String> events = new ArrayList<>();
-        events.add("Fun Mudder");
-        events.add("Rise and Grind");
-        events.add("The Road to Victory");
-        model.addAttribute("events", "events");
+@GetMapping("/")
+    public String displayAllEvents(Model model){
+        model.addAttribute("events", events);
         return "events/index";
     }
     //lives at /events/create
@@ -27,6 +27,12 @@ public class EventController {
     public String renderCreateEventForm(){
     return "events/create";
     }
-
+    // method below handles a different kind of request and can be placed at the same path
+@PostMapping("create")
+public String createEvent(@RequestParam String eventName){
+        events.add(eventName);
+        //the return redirects to the root path, which is events
+        return "redirect: ";
+    }
 
 }
