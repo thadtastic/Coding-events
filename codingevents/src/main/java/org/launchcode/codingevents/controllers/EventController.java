bb82c759067498a0a7241f8458fd734d8fda6cee
thadtastic <leothaddeus92@gmail.com>
2024-02-11 11:44:42 -0600

    created and render form

--- codingevents/src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ codingevents/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -22,4 +22,11 @@ public class EventController {
         model.addAttribute("events", "events");
         return "events/index";
     }
+    //lives at /events/create
+    @GetMapping("create")
+    public String renderCreateEventForm(){
+    return "events/create";
+    }
+
+
 }
